504aac5d9c5c130cf061a685ffb615d19918d964
Vipen <vipenmahay@gmail.com>
2021-05-22 16:58:22 +0100

    slim installation

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -44,6 +44,20 @@ use OCA\FaceRecognition\Listener\LoadSidebarListener;
 use OCA\FaceRecognition\Listener\UserDeletedListener;
 
 use OCA\FaceRecognition\Search\PersonSearchProvider;
+use Slim\Factory\AppFactory;
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Hello world!");
+    return $response;
+});
+
+$app->run();
 
 class Application extends App implements IBootstrap {
 
