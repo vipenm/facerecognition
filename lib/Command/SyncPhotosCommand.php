005e321b3944b179dd4fd4872d0329b3e87f84c3
Vipen <vipenmahay@gmail.com>
2021-05-23 16:00:42 +0100

    test background job

---
+++ lib/Command/SyncPhotosCommand.php
@@ -0,0 +1,146 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OCA\FaceRecognition\Command;
+
+use OCP\Files\IRootFolder;
+use OCP\App\IAppManager;
+use OCP\IConfig;
+use OCP\IUserManager;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use OCA\FaceRecognition\BackgroundJob\BackgroundService;
+
+class SyncPhotosCommand extends Command {
+
+	/** @var BackgroundService */
+	protected $backgroundService;
+
+	/** @var IUserManager */
+	protected $userManager;
+
+	/**
+	 * @param BackgroundService $backgroundService
+	 * @param IUserManager $userManager
+	 */
+	public function __construct(BackgroundService $backgroundService,
+	                            IUserManager      $userManager) {
+		parent::__construct();
+
+		$this->backgroundService = $backgroundService;
+		$this->userManager = $userManager;
+	}
+
+	protected function configure() {
+		$this
+			->setName('face:sync_photos')
+			->setDescription('Run\s the photo server sync')
+			->addOption(
+				'user_id',
+				'u',
+				InputOption::VALUE_REQUIRED,
+				'Analyze faces for the given user only. If not given, analyzes images for all users.',
+				null
+			)
+			->addOption(
+				'max_image_area',
+				'M',
+				InputOption::VALUE_REQUIRED,
+				'Caps maximum area (in pixels^2) of the image to be fed to neural network, effectively lowering needed memory. ' .
+				'Use this if face detection crashes randomly.'
+			)
+			->addOption(
+				'defer-clustering',
+				null,
+				InputOption::VALUE_NONE,
+				'Defer the face clustering at the end of the analysis to get persons in a simple execution of the command.'
+			)
+			->addOption(
+				'timeout',
+				't',
+				InputOption::VALUE_REQUIRED,
+				'Sets timeout in seconds for this command. Default is without timeout, e.g. command runs indefinitely.',
+				0
+			);
+	}
+
+	/**
+	 * @param InputInterface $input
+	 * @param OutputInterface $output
+	 * @return int
+	 */
+	protected function execute(InputInterface $input, OutputInterface $output) {
+		return "Hello world";die;
+		$this->backgroundService->setLogger($output);
+
+		// Extract user, if any
+		//
+		$userId = $input->getOption('user_id');
+		$user = null;
+
+		if (!is_null($userId)) {
+			$user = $this->userManager->get($userId);
+			if ($user === null) {
+				throw new \InvalidArgumentException("User with id <$userId> in unknown.");
+			}
+		}
+
+		// Extract timeout
+		//
+		$timeout = $input->getOption('timeout');
+		if (!is_null($timeout)) {
+			if ($timeout < 0) {
+				throw new \InvalidArgumentException("Timeout must be positive value in seconds.");
+			}
+		} else {
+			$timeout = 0;
+		}
+
+		// Extract max image area
+		//
+		$maxImageArea = $input->getOption('max_image_area');
+		if (!is_null($maxImageArea)) {
+			$maxImageArea = intval($maxImageArea);
+
+			if ($maxImageArea === 0) {
+				throw new \InvalidArgumentException("Max image area must be positive number.");
+			}
+
+			if ($maxImageArea < 0) {
+				throw new \InvalidArgumentException("Max image area must be positive value.");
+			}
+		}
+
+		// Extract defer clustering option
+		//
+		$deferClustering = $input->getOption('defer-clustering');
+
+		// Extract verbosity (for command, we don't need this, but execute asks for it, if running from cron job).
+		//
+		$verbose = $input->getOption('verbose');
+
+		// Main thing
+		//
+		$this->backgroundService->execute($timeout, $verbose, $user, $maxImageArea, $deferClustering);
+
+		return 0;
+	}
+}
